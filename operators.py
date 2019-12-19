945c14b56c8943eba7488b1bc67b59d5a5e33480
boma <mabo1996@outlook.com>
2019-12-19 09:52:23 +0800

    添加多期获取残差的函数

--- operators.py
+++ operators.py
@@ -32,19 +32,22 @@ def ts_delta(data,period):
     delta = data - pre_data
     return delta
 
-# todo 回归除了获取回归系数之外 至少还要获取 残差 需要进行完善
-def all_period_cross_multi_regression(datay,*args):
-    paramList = []
-    for i in range(datay.shape[0]):
-        x = []
-        for datax in args:
-            x.append(datax[i])
-        x = np.vstack(x)
-        y = datay[i]
-        params = multi_regression(y,x)
-        paramList.append(params)
-    paramList = np.array(paramList)
-    return paramList
+def corr_vet(vet1,vet2):
+    # 计算两个向量的相关性 如果存在nan值怎么处理 两个向量的nan值所在的位置不同
+    # 取两个vect都不为nan的值
+    notNullIndex = ~(np.isnan(vet1) or np.isnan(vet2))
+    new_vet1 = vet1[notNullIndex]
+    new_vet2 = vet2[notNullIndex]
+    corr = np.corrcoef(new_vet1,new_vet2)[0,1]
+    return corr
+
+def corr_mat(mat1,mat2):
+    # 计算两个矩阵 对应的截面的相关性
+    corrs = []
+    for i in range(len(mat1)):
+        corrs.append(corr_vet(mat1[i],mat2[i]))
+    corrs = np.array(corrs)
+    return corrs
 
 def all_period_cross_simple_regression(datay,datax):
     # 进行单元回归 每一期进行回归
@@ -66,12 +69,10 @@ def simple_regression(datay,datax):
     results = model.fit()
     return results.params
 
-
 def simple_regression_resid(datay, datax):
     # 单元回归 一期
     x = datax
     y = datay
-
     not_nan_flag = ~(np.isnan(x * y))
     not_nan_index = np.arange(len(not_nan_flag))[ not_nan_flag ]
 
@@ -87,6 +88,30 @@ def simple_regression_resid(datay, datax):
 
     return resid_result
 
+def all_period_simple_regression_resid(datay, datax):
+    # 单元多期的残差获取
+    resid_mat = np.zeros_like(datay)
+    resid_mat[:] = np.nan
+
+    for i in range(datay.shape[0]):
+        x = datax[i]
+        y = datay[i]
+
+        not_nan_flag = ~(np.isnan(x * y))
+        not_nan_index = np.arange(len(not_nan_flag))[not_nan_flag]
+
+        new_x = x[not_nan_flag]
+        new_y = y[not_nan_flag]
+        X = sm.add_constant(new_x.T)
+        model = sm.OLS(new_y, X)
+        results = model.fit()
+
+        resid_result = np.zeros_like(y)
+        resid_result[:] = np.nan
+        resid_result[not_nan_index] = results.resid
+        resid_mat[i] = resid_result
+    return resid_mat
+
 def multi_regression(datay,datax):
     # 进行单元回归 一期
     x = datax
@@ -96,6 +121,18 @@ def multi_regression(datay,datax):
     results = model.fit()
     return results.params # 参数为常数开始 其余的对应datax的顺序
 
+def all_period_cross_multi_regression(datay,*args):
+    paramList = []
+    for i in range(datay.shape[0]):
+        x = []
+        for datax in args:
+            x.append(datax[i])
+        x = np.vstack(x)
+        y = datay[i]
+        params = multi_regression(y,x)
+        paramList.append(params)
+    paramList = np.array(paramList)
+    return paramList
 
 def multi_regression_resid(datay, *args):
     # 进行多元回归 一期
@@ -120,23 +157,32 @@ def multi_regression_resid(datay, *args):
     return resid_result
 
 
-def corr_mat(mat1,mat2):
-    # 计算两个矩阵 对应的截面的相关性
-    corrs = []
-    for i in range(len(mat1)):
-        corrs.append(corr_vet(mat1[i],mat2[i]))
-    corrs = np.array(corrs)
-    return corrs
+def all_period_multi_regression_resid(datay, *args):
+    # 单元多期的残差获取
+    resid_mat = np.zeros_like(datay)
+    resid_mat[:] = np.nan
 
-def corr_vet(vet1,vet2):
-    # 计算两个向量的相关性 如果存在nan值怎么处理 两个向量的nan值所在的位置不同
-    # 取两个vect都不为nan的值
-    notNullIndex = ~(np.isnan(vet1) or np.isnan(vet2))
-    new_vet1 = vet1[notNullIndex]
-    new_vet2 = vet2[notNullIndex]
-    corr = np.corrcoef(new_vet1,new_vet2)[0,1]
-    return corr
+    for i in range(datay.shape[0]):
+        x = []
+        for datax in args:
+            x.append(datax[i])
+        x = np.vstack(x)
+        y = datay[i]
+        not_nan_flag = ~np.any(np.isnan(x * y), axis=0)
+        not_nan_index = np.arange(len(not_nan_flag))[not_nan_flag]
 
+        x = x.T
+        new_x = x[not_nan_flag]
+        new_y = y[not_nan_flag]
+        X = sm.add_constant(new_x)
+        model = sm.OLS(new_y, X)
+        results = model.fit()
+
+        resid_result = np.zeros_like(y)
+        resid_result[:] = np.nan
+        resid_result[not_nan_index] = results.resid
+        resid_mat[i] = resid_result
+    return resid_mat
 
 def optimazier(data,*args):
 
